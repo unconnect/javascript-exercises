2929c48a678518c7b18656da21ac24e5aed62f70
Alexander Nikolas Reuber <info@unconnect.net>
2022-03-01 22:40:26 +0100

    finished removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(theArray, ...argsToRemove) {
+    // Iterate over all arguments that should be remove from the array.
+    // In the inner loop we test each element if it is equal to ohne of the arguments to remove
 
+    // Filters array with array with includes method
+    return theArray.filter((elementToCheck) => !argsToRemove.includes(elementToCheck));
 };
 
 // Do not edit below this line
