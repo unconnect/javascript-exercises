9d13819b7e42ffafdbc3934008f60d1c521c7f87
Alexander Nikolas Reuber <info@unconnect.net>
2022-03-01 20:20:07 +0100

    finished helloWorld, repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(stringToRepeat, numberToRepeatString) {
+    const _stringToRepeat = stringToRepeat;
+    const _numberToRepeatString = numberToRepeatString;
+    const _theStrings = []
+    
+    for (let index = 0; index < _numberToRepeatString; index++) {
+        _theStrings.push(_stringToRepeat);
+    }
+    
+    return (_numberToRepeatString === -1) ? 'ERROR' : _theStrings.join('');
 };
 
 // Do not edit below this line
