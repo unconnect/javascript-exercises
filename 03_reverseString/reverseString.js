baae92789623eafee8ee8066b8de22fa0fb9d1fa
Alexander Nikolas Reuber <info@unconnect.net>
2022-03-01 20:57:38 +0100

    finished reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(stringToReverse) {
+    // const array_stringToReverse = stringToReverse.split(); // Breaks with unicode chars
+    // one of some solutions
+    // see: https://stackoverflow.com/questions/4547609/how-to-get-character-array-from-a-string/34717402#34717402
+    const array_stringToReverse = [...stringToReverse];
+    return array_stringToReverse.reverse().join('');
 };
 
 // Do not edit below this line
