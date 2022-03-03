aef6673a5b4d626aebdfd313f43302e1d64cb2a7
Alexander Nikolas Reuber <info@unconnect.net>
2022-03-03 21:57:23 +0100

    finiseh sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(number1, number2) {
 
+    // Check for smaller 0 or not-a-number
+    if (number1 < 0 || number2 < 0 || typeof number1 !== 'number' || typeof number2 !== 'number') {
+        return 'ERROR';
+    }
+
+    // Checks for larger number first and swiches them
+    if(number1 > number2) {
+        let dummy = number2;
+        number2 = number1;
+        number1 = dummy;
+    }
+
+    let sum = 0;
+    let index = number1;
+
+    while (index <= number2) {
+        sum += index;
+        index++;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
