5bc4b4ba96ae999ef18b55e2486d6225100e5ebe
Alexander Nikolas Reuber <info@unconnect.net>
2022-03-13 08:19:21 +0100

    also tried the boolean expression solution

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,9 +1,16 @@
 const leapYears = function(givenYear) {
+    
+    // Solution with ifs
+    
     // years divisible by 4 are no leap years
-    if(givenYear % 4 !== 0) return false;
+    // if(givenYear % 4 !== 0) return false;
     // century years are no leap years, unless divisible by 400
-    if(givenYear % 100 === 0 && givenYear % 400 !== 0) return false;
-    return true;
+    // if(givenYear % 100 === 0 && givenYear % 400 !== 0) return false;
+    // return true;
+
+    // very elegantly solution with boolean expression
+    return givenYear % 4 === 0 && (givenYear % 100 !== 0 || givenYear % 400 === 0);
+
 };
 
 // Do not edit below this line
