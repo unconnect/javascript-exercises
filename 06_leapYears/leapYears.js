bdf22c87d7c8724e71c2e761e81f08e2b42e03ca
Alexander Nikolas Reuber <info@unconnect.net>
2022-03-13 08:11:38 +0100

    leapYears finished

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function(givenYear) {
+    // years divisible by 4 are no leap years
+    if(givenYear % 4 !== 0) return false;
+    // century years are no leap years, unless divisible by 400
+    if(givenYear % 100 === 0 && givenYear % 400 !== 0) return false;
+    return true;
 };
 
 // Do not edit below this line
