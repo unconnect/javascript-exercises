72f9b63616c06720331bb9c4b1b0311dcf1b428e
Alexander Nikolas Reuber <info@unconnect.net>
2022-09-11 09:17:25 +0200

    finished calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,26 @@
-const add = function() {
-	
+const add = function (number1, number2) {
+  return number1 + number2
 };
 
-const subtract = function() {
-	
+const subtract = function(number1, number2) {
+  return number1 - number2
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	return numbers.reduce((total, currentVal) => total + currentVal, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+	return numbers.reduce((total, currentVal) => total * currentVal, 1);
 };
 
-const power = function() {
-	
+const power = function(number1, number2) {
+  return number1 ** number2
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  if(number === 0) return 1
+	return number * factorial(number - 1)
 };
 
 // Do not edit below this line
