6c5f41e4cc992b39915272e0d185ce11685d9739
Alexander Nikolas Reuber <info@unconnect.net>
2022-09-11 11:19:33 +0200

    finished palindrome exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  const regEx = /[\s\,\.\!\?\']/gi;
+  const mergedString = string.toLowerCase().replaceAll(regEx, "");
+  const reversedString = Array.from(mergedString).reverse().join("");
+  return mergedString === reversedString;
 };
-
 // Do not edit below this line
 module.exports = palindromes;
