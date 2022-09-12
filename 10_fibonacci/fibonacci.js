3637ee852449ae820461da948ef43e7de3cdeda5
Alexander Nikolas Reuber <info@unconnect.net>
2022-09-12 21:05:00 +0200

    finished fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,7 @@
-const fibonacci = function() {
-
+const fibonacci = function(fibMember) {
+  if (fibMember < 0) return 'OOPS'
+  if (fibMember <= 1) return Number(fibMember); 
+  return fibonacci(fibMember - 1) + fibonacci(fibMember - 2);
 };
 
 // Do not edit below this line
