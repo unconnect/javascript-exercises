468001fa51409a7737a49d91ddabdf68ff98f567
Alexander Nikolas Reuber <info@unconnect.net>
2022-09-12 22:20:33 +0200

    fiished findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people = {}) {
+  return people.reduce((prevPerson, currentPerson) => {
+    if (currentPerson["yearOfDeath"] === undefined)
+    currentPerson["yearOfDeath"] = new Date().getFullYear();
+    if (prevPerson.name === undefined) return currentPerson
+    if(prevPerson.yearOfDeath - prevPerson.yearOfBirth <
+      currentPerson.yearOfDeath -
+      currentPerson.yearOfBirth) prevPerson = currentPerson;
+    return prevPerson
+  }, {})
 };
 
 // Do not edit below this line
